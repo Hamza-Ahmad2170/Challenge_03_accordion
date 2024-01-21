9396d54d074baa5a645034969a1fd10d53928efe
hasaigal <hasaigal@gmail.com>
2024-01-22 04:01:12 +0500

    react

---
+++ src/Output.jsx
@@ -0,0 +1,9 @@
+function OutPut({ bill, tip }) {
+  return (
+    <h2>
+      You pay {bill + tip} (${bill} + ${tip} tip)
+    </h2>
+  );
+}
+
+export default OutPut;
---
+++ src/Reset.jsx
@@ -0,0 +1,5 @@
+function Reset({ onHandleRest }) {
+  return <button onClick={onHandleRest}>Reset</button>;
+}
+
+export default Reset;
